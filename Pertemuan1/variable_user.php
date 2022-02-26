4f8b41e817369a0b0081740c4c5fcc9e46ef419a
Zahra Mumtazah <91921976+Mumtazahra@users.noreply.github.com>
2022-02-26 11:54:57 +0700

    Add files via upload

---
+++ Pertemuan1/variable_user.php
@@ -0,0 +1,14 @@
+<?php
+//definisikan variable
+$nama = "Rosalie Naurah";
+$umur = 21;
+$berat = 39.5;
+
+echo "Nama : $nama ";
+echo "<br/>Umur : $umur Tahun ";
+echo "<br/>Berat : $berat Kg ";
+
+echo "<br/> Hello $nama Apa kabar? ";
+echo "<br/> Hai Nama saya $nama umur saya $umur ";
+echo "<hr>";
+?>
