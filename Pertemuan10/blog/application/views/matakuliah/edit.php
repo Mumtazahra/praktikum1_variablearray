7425a5bf8c8a9e881ff7e13b3f8be117eeeb4bde
Zahra Mumtazah <91921976+Mumtazahra@users.noreply.github.com>
2022-05-30 13:20:24 +0700

    Pertemuan10

---
+++ Pertemuan10/blog/application/views/matakuliah/edit.php
@@ -0,0 +1,32 @@
+<div class="container">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<h1>Form Input Mata Kuliah</h1>
+</br>
+<?php echo form_open("matakuliah/save")?>
+  <div class="form-group row">
+    <label for="nama" class="col-2 col-form-label">Nama</label> 
+    <div class="col-4">
+      <input id="nama" name="nama" placeholder="Masukkan Nama MatKul" type="text" value="<?php echo $obj_matakuliah->nama?>" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="sks" class="col-2 col-form-label">Jumlah SKS</label> 
+    <div class="col-4">
+      <input id="sks" name="sks" placeholder="Masukkan Jumlah SKS" type="text" value="<?php echo $obj_matakuliah->sks?>"  class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="kode" class="col-2 col-form-label">Kode Matkul</label> 
+    <div class="col-4">
+      <input id="kode" name="kode" placeholder="Masukkan Kode Matkul" type="text" value="<?php echo $obj_matakuliah->kode?>" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-2 col-4">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+<?php echo form_close()?>
+</div>
---
+++ Pertemuan10/blog/application/views/matakuliah/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mata Kuliah</title>
+    <script> 
+        function hapusMatakuliah(pesan){
+            if (confirm(pesan)){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    </script>
+</head>
+<?php
+$username = $this->session->userdata('username');
+if($username) {
+    echo "<h2>Selamat Datang $username</h2>";
+}
+?>
+<body>
+    <div class="col-md-12">
+    <h3>Mata Kuliah<h3>
+    <table border="1" class="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>SKS</th>
+                <th>Kode</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $nomor = 1;
+            foreach ($matakuliah as $mkh) {
+            ?>
+            <tr>
+                <td><?php echo $nomor ?></td>
+                <td><?php echo $mkh -> nama ?></td>
+                <td><?php echo $mkh -> sks ?></td>
+                <td><?php echo $mkh -> kode ?></td>
+                <td>
+                <a href=<?php echo base_url("index.php/matakuliah/edit/$mkh->id") ?>
+                    class="btn btn-success btn-lg active">Edit</a>
+                    &nbsp;
+                    <a href=<?php echo base_url("index.php/matakuliah/delete/$mkh->id") ?>
+                    class="btn btn-danger btn-lg active" onclick="return hapusMatakuliah('Anda yakin ingin menghapus matakuliah  <?php echo $mkh-> nama ?> ?')">Hapus</a>   
+                </td>
+            </tr>
+            <?php
+            $nomor++;
+            }
+            ?>
+        </tbody>
+    </table>
+    <a href= <?php echo base_url("index.php/matakuliah/form") ?> class="btn btn-primary btn-lg active">Tambah</a>
+    </div>
+</body>
+</html>
